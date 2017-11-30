ad418417beef17655759763af99a2776d4e35248
Paritosh Yadav <paritosh.yadav@ixc.com>
2017-11-30 14:50:54 +0600

    decimalEncoder file edit

--- rotation_round/decimalencoder.py
+++ rotation_round/decimalencoder.py
@@ -1,6 +1,10 @@
+# """
+# Date:-28-Nov-17
+# created by:- paritosh yadav
+# description:- This class use to convert string object to decimal help to access data from AWS.
+# """
 import decimal
 import json
-
 
 # This is a workaround for: http://bugs.python.org/issue16535
 class DecimalEncoder(json.JSONEncoder):
